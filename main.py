730335bbd12eb19e739571375cd61e2d4d9ab8f9
Nanosuit <jabhim06@gmail.com>
2026-08-28 21:13:00 +0530

    changes on 18082026

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import tkinter as tk
+
+
+
+def display_image(image1):
+    label.config(image=image1)  # Update label text to indicate image capture
+    label.image = image1        # Keep a reference to avoid garbage collection
+
+
+def capture_image():
+    # Placeholder function for capturing an image
+
+    image1 = tk.PhotoImage(file="Test_Image1.png")
+    display_image(image1)
+    print("Capture Image button clicked!")
+    # Here you would add the code to interface with the Raspberry Pi camera and capture an image
+
 # 1. Initialize the main application window
 root = tk.Tk()
 
@@ -24,6 +40,15 @@ filemenu.add_command(label="Exit", command=root.quit)
 helpmenu = tk.Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About")
+
+#####DISPLAY WINDOW#####
+image = tk.PhotoImage(file="Test_Image.png")
+label = tk.Label(root, image=image, width=600, height=500)
+label.place(x=5, y=10)
+
+#####ALL THE BUTTONS#####
+button = tk.Button(root, text="Capture Image", width=25, command=capture_image)
+button.place(x=610, y=10)
 
 # 5. Start the event loop to keep the window open
 root.mainloop()
