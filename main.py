73ca8cde2bc16997923b45ab42d464151d53a501
Nanosuit <jabhim06@gmail.com>
2026-08-29 22:01:11 +0530

    changes 29082026

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 import tkinter as tk
+import time
+from time import sleep
+from PIL import Image, ImageTk
+from picamera2 import Picamera2, Preview
 
+first = 0
+picam2 = Picamera2()
 
-
+def close_camera():
+    global picam2
+    print('Closing Camera...')
+    picam2.close()  # Releases the hardware resource back to the OS
+    
 def display_image(image1):
     label.config(image=image1)  # Update label text to indicate image capture
     label.image = image1        # Keep a reference to avoid garbage collection
 
-
 def capture_image():
-    # Placeholder function for capturing an image
-
-    image1 = tk.PhotoImage(file="Test_Image1.png")
-    display_image(image1)
+    global first
+    global picam2
+    picam2.start()
+    time.sleep(1)
+    variable = (time.strftime("%y-%b-%d_%H:%M:%S"))
+    picam2.capture_file('Test_Image1'+variable+'.png')
+    picam2.stop()
     print("Capture Image button clicked!")
     # Here you would add the code to interface with the Raspberry Pi camera and capture an image
 
+def start_camera():
+    global picam2
+    while 1:
+        picam2.start()
+        time.sleep(0.05)
+        frame = picam2.capture_array()
+        print(frame.shape)
+        image1 = Image.fromarray(frame)
+        image1 = ImageTk.PhotoImage(image1)
+        display_image(image1)
+        picam2.stop()
+        root.update()
+    
 # 1. Initialize the main application window
 root = tk.Tk()
 
@@ -47,8 +72,12 @@ label = tk.Label(root, image=image, width=600, height=500)
 label.place(x=5, y=10)
 
 #####ALL THE BUTTONS#####
-button = tk.Button(root, text="Capture Image", width=25, command=capture_image)
+button = tk.Button(root, text="Start Camera", width=25, command=start_camera)
 button.place(x=610, y=10)
-
+button = tk.Button(root, text="Capture Image", width=25, command=capture_image)
+button.place(x=610, y=50)
+button = tk.Button(root, text="Stop Camera", width=25, command=close_camera)
+button.place(x=610, y=100)
 # 5. Start the event loop to keep the window open
 root.mainloop()
+print('Program exiting....Releasing all the harware.')
---
+++ rpi_utils.py
@@ -0,0 +1,4 @@
+def open_camera():
+    # Placeholder function for opening the camera
+    
+    # Here you would add the code to interface with the Raspberry Pi camera and open it
